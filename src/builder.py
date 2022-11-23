8765dc27a02d8301d84e6f65d0e7662baeb935fb
MajongDa <dolkhowsky@gmail.com>
2022-11-23 22:58:50 +0300

    Небольшое исправление

--- src/builder.py
+++ src/builder.py
@@ -1,32 +1,23 @@
+#lib is path to testing library sources
+#file is path to generated test*.cpp
+#nanobench is path to nanobench header, most likely is '.'
+#look at the previos commits for debugging module
+
 import os
 import subprocess
+import sys
 
 COMP = 'g++'
 OPTIMISATION = ['-O0', '-O1', '-O2', '-O3', '-Ofast']
-NANOBENCH = '.'
 
 def find_files():
 	files = [entry.name for entry in os.scandir(os.getcwd()) if entry.name.startswith('test') and entry.name.endswith('.cpp') and entry.is_file() and not entry.name == 'nanobench.h']
 	for f in files: print(f)
 	return files
 
-def build(lib):
-	src = 'SRCMODULES = ' + ' '.join(find_files()) + '\n'
-	#subprocess.run(['g++', '-I.', 'lsm.cpp', 'testcubik.cpp', '-O0',\
-	#	 '-o testcubikO0'])
-	optim = 'OPTIMIZATION = ' + ' '.join(OPTIMISATION) + '\n'
-	for file in find_files():
-		for oX in OPTIMISATION:
-			print(COMP, '-I'+NANOBENCH, lib, file,\
-			oX, '-o', file[:len(file)-4]+oX[1:])
-			subprocess.run([COMP, '-I'+NANOBENCH, lib, file,\
-				oX, '-o', file[:len(file)-4]+oX[1:]])
-	print(src)
 
-			
-def main():
-	build('lsm.cpp')
-
-
-if __name__ == '__main__':
-	main()
+def build(lib, file, nanobench = '.'):
+	#for file in find_files():
+	for oX in OPTIMISATION:
+		subprocess.run([COMP, '-I'+nanobench, lib, file,\
+			oX, '-o', file[:len(file)-4]+oX[1:]])
