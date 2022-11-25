f15c62aeb15a554e56ee3da902a86b235ce1a8f2
MajongDa <dolkhowsky@gmail.com>
2022-11-25 21:54:56 +0300

    Пример

---
+++ src/example.py
@@ -0,0 +1,4 @@
+import os, string, sys, subprocess
+
+subprocess.run(['python3', 'generator.py', '/home/majong/PEAS/src/'])
+subprocess.run(['python3', 'builder.py', '/home/majong/PEAS/src/lsm.cpp', '/home/majong/PEAS/src/'])
