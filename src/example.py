8b13cd66491c8b837c78a6913b098c335fffafd9
hctilf <dimabro180@gmail.com>
2022-11-30 01:15:22 +0300

    (fixes) new feats in build_threads and parse_jsons

--- src/example.py
+++ src/example.py
@@ -7,4 +7,4 @@ subprocess.run(['python3', 'generator.py', cur_dir])
 lib_name = os.getcwd() + os.sep + glob.glob("**/**/lsm.cpp", recursive=True)[0]
 #or use this "$(find -iname 'lsm.cpp')"
 #generator needs two additional argument: first is path to library sources, the second one is parent directory to testSOMETHING.cpp 
-subprocess.run(['python3', 'builder.py', lib_name, cur_dir])
+subprocess.run(['python3', 'builder_threads.py', lib_name, cur_dir])
