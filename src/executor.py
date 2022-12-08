2a444baa855cbd851eb9c510e299e16ac8fcded0
MajongDa <dolkhowsky@gmail.com>
2022-12-08 23:37:41 +0300

    Исправлены директории

--- src/executor.py
+++ src/executor.py
@@ -1,24 +1,16 @@
-import os, sys, subprocess, psutil, threading
-from  generator import *
-#generator needs one additional argument: .h file parent directory
-#subprocess.run(['python3', 'generator.py', '/home/majong/PEAS/src/'])
+import os, subprocess
 
 f = open('/sys/devices/system/cpu/cpu0/cache/index3/size', 'r')
 l3 = int((f.readline().replace('K', '')))*1024
 f.close()
-print(l3)
-mem_req = generator('/home/majong/PEAS/src/')
-
-#generator needs one additional argument: first is path to library sources, the second one is parent directory to testSOMETHING.cpp 
-#subprocess.run(['python3', 'builder.py', '/home/majong/PEAS/src/lsm.cpp', '/home/majong/PEAS/src/'])
 
 
-class execute():
-	def __init__(self, id, folder) -> None:
-		self.mem_req = int(l3/(2**mem_req[id][1]+4**mem_req[id][2]+8**mem_req[id][3]))
+class executable():
+	def __init__(self, id, info) -> None:
+		self.mem_req = int(l3/(2**info[1]+4**info[2]+8**info[3]))
 		self.id = id
-		self.testing = folder
-		self.mainDir = os.path.split(os.getcwd())[0]
+		self.testing = info[0]
+		self.mainDir = f'/home/{os.getlogin()}/PEAS'
 		self.tmpDir = self.mainDir + '/tmp'
 		self.wd = self.tmpDir + f'/{self.testing}'
 		self.checkForTmpNdata()
@@ -41,13 +33,11 @@ class execute():
 				subprocess.run(['./' + exec, str(self.mem_req)])
 		os.chdir(self.tmpDir)
 
-def main():
-	for id, dir in enumerate(mem_req):
+def execute(mem_req):
+	for id, info in enumerate(mem_req):
 		print('Your cpu L3 cashe size(in Bt)', l3)
-		exe = execute(id, mem_req[id][0])
+		exe = executable(id, info)
 		print('Current executable n = ', exe.mem_req)
 		
 
-if __name__ == "main":
-	main()
 #exe = execute()
