77f609e728fd2577e65d17927cc07319c98fec4e
vadim <tsoy086.90@mail.ru>
2022-12-08 14:30:54 +0300

    Multithread builder

--- src/builder_threads.py
+++ src/builder_threads.py
@@ -7,39 +7,80 @@ import os
 import subprocess
 import sys
 import threading
+import time
 
 COMP = 'g++'
 OPTIMISATION = ['-O0', '-O1', '-O2', '-O3', '-Ofast']
-THREADS = []
 
+buildMutex = threading.Lock()
+cpuCnt = os.cpu_count()
+curDir = os.getcwd()
+parDir = os.path.split(curDir)[0]
 lib = sys.argv[1]
+threadC = []
 
+def time_prog(func):	#time measurement
+	def wrapper(*args):
+		start = time.time()
+		func(*args)
+		print(time.time() - start)
+	return wrapper
 
-class buildThread(threading.Thread):
-	def __init__(self, id, task) -> None:
+class buildThreadMain(threading.Thread):
+	def __init__(self, id, mutex,task) -> None:
 		self.id = id
+		self.mutex = mutex
+		self.file = task
+		threading.Thread.__init__(self)
+
+	def run(self):
+		if cpuCnt <= 4:
+			with self.mutex:
+				for id, oX in enumerate(OPTIMISATION):
+					threadC.append([])
+					thread = buildThreadChild(id, oX, buildMutex, self.file)
+					thread.start()
+					threadC[id].append(thread)
+		else:
+			for id, oX in enumerate(OPTIMISATION):
+				threadC.append([])
+				thread = buildThreadChild(id, oX, buildMutex, self.file)
+				thread.start()
+				threadC[id].append(thread)
+
+class buildThreadChild(threading.Thread):
+	def __init__(self, id, oX, mutex, task) -> None:
+		self.id = id
+		self.oX = oX
+		self.mutex = mutex
 		self.file = task
 		threading.Thread.__init__(self)
 
 	def run(self):
 		global lib
-		for oX in OPTIMISATION:
+		if cpuCnt <= 2:
+			with self.mutex:
+				subprocess.run([COMP, '-I.', lib, self.file,\
+					self.oX, '-o', f"{parDir}/tmp/{self.file[:-4]}/{self.file[:len(self.file)-4]}{self.oX[1:]}"])
+		else:
 			subprocess.run([COMP, '-I.', lib, self.file,\
-				oX, '-o', self.file[:len(self.file)-4]+oX[1:]])
+				self.oX, '-o', f"{parDir}/tmp/{self.file[:-4]}/{self.file[:len(self.file)-4]}{self.oX[1:]}"])	
 
 def find_files():
 	files = [entry.name for entry in os.scandir(os.getcwd()) if entry.name.startswith('test') and entry.name.endswith('.cpp') and entry.is_file() and not entry.name == 'nanobench.h']
 	for f in files: print(f)
 	return files
 
-
+@time_prog
 def build(lib, file, nanobench = '.'):
+	if not os.path.exists(f"{parDir}/tmp"):
+		os.mkdir(f"{parDir}/tmp")
 	for id, file in enumerate(find_files()):
-		thread = buildThread(id, file)
+		if not os.path.exists(f"{parDir}/tmp/{file[:-4]}"):
+			os.mkdir(f"{parDir}/tmp/{file[:-4]}")
+		thread = buildThreadMain(id, buildMutex, file)
 		thread.start()
-		THREADS.append(thread)
+	for threadList in threadC:
+		for thread in threadList: thread.join()
 
 build(sys.argv[1], sys.argv[2])
-
-for thread in THREADS:
-    thread.join()
