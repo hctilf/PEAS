381c410c5585b7c75b6d96272d30db751394d3ae
MajongDa <dolkhowsky@gmail.com>
2022-12-24 13:36:54 +0300

    shizik

--- src/lsm.h
+++
@@ -1,4 +0,0 @@
-double* square(int , double*, double*);
-double* cubik(int ,double*, double*);
-void errors(int, double*, double*, double*,  double&, double&, double&, double&);
-int gauss(int, double**, double*, double*);
